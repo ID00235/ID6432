cf5c42a7e9c550385e1a085115acd244da6f4e4d
Irwan Kurniawan <irwanka.email@gmail.com>
2017-05-30 22:01:04 +0700

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,14 @@ Route::get('login', "LoginController@login");
 Route::get('logout', "LoginController@logout");
 Route::post('submit-login', "LoginController@submitlogin");
 
+
+Route::get('generate-form', "GenerateController@generateform");
+
 Route::get('form/baru/{table_name}', function($table_name){
 	$columns = DB::select('show columns from '.$table_name);
 	return view('form-builder',array("columns"=>$columns,"table_name"=>$table_name));
+});
+Route::get('form/edit/{table_name}', function($table_name){
+	$columns = DB::select('show columns from '.$table_name);
+	return view('edit-builder',array("columns"=>$columns,"table_name"=>$table_name));
 });
--- resources/views/edit-builder.blade.php
+++ resources/views/edit-builder.blade.php
@@ -1,6 +1,5 @@
-<b>SCRIPT FORM DI BLADE</b>
-<hr>
-&#123!! Form::open(['url' => 'route-ke-submit-form']) !!&#125<br>
+&#60;form action="&#123;&#123;URLGroup('sesuaikan-dengan-nama-route-post')&#125;&#125;" id="form-{{$table_name}}" method="post"&#62; <br>
+&#123;&#123;csrf_field()&#125;&#125;<br>
 @foreach ($columns as $value)
 <?php
 	$type = $value->Type;
@@ -8,16 +7,19 @@
 	$required = $value->Null == "NO" ? "'required'=>true" : "";
 	$primary = $value->Key=="PRI" ? 1 : 0;
 ?>
+@if($primary)
+<?php echo '{{';?>Form::hidden("{{$field}}",Crypt::encrypt($data->{{$field}}))<?php echo '}}';?><br>
+@endif
 @if(!$primary && $field!='created_at' && $field!='updated_at')
 	@if(substr($type,0,7)=='varchar' || substr($type,0,3)=='int' )
-	<?php echo '{{';?>Form::bsText("{{$field}}","",[<?php echo $required;?>])<?php echo '}}';?><br>
+	<?php echo '{{';?>Form::bsText("{{$field}}","&#123;&#123;$data->{{$field}}&#125;&#125;",[<?php echo $required;?>])<?php echo '}}';?><br>
 	@elseif(substr($type,0,7)=='decimal')
-	<?php echo '{{';?>Form::bsText("{{$field}}","",['class'=>'col-4 double input-right form-control',
+	<?php echo '{{';?>Form::bsText("{{$field}}","&#123;&#123;$data->{{$field}}&#125;&#125;",['class'=>'col-4 double input-right form-control',
 	<?php echo $required;?>])<?php echo '}}';?><br>
 	@elseif(substr($type,0,6)=='double')
-	<?php echo '{{';?>Form::bsText("{{$field}}","",['class'=>'col-4  input-right form-control',<?php echo $required;?>])<?php echo '}}';?><br>
+	<?php echo '{{';?>Form::bsText("{{$field}}","&#123;&#123;$data->{{$field}}&#125;&#125;",['class'=>'col-4  input-right form-control',<?php echo $required;?>])<?php echo '}}';?><br>
 	@elseif(substr($type,0,4)=='date')
-	<?php echo '{{';?>Form::bsText("{{$field}}","",['class'=>'col-4 datepicker form-control',<?php echo $required;?>])<?php echo '}}';?><br>
+	<?php echo '{{';?>Form::bsText("{{$field}}","&#123;&#123;tanggalIndo($data->{{$field}})&#125;&#125;",['class'=>'col-4 datepicker form-control',<?php echo $required;?>])<?php echo '}}';?><br>
 	@elseif(substr($type,0,4)=='enum')
 	 <?php
 	   $type = str_replace("enum(", "", $type);
@@ -31,7 +33,7 @@
 	  &#60;{{"?php"}}<br>
 	  <?php
 	   echo '$list = array('.$string_array.');<br>';
-	   echo '$select ='.$type[0].';<br>';
+	   echo '$select =';?>$data-><?php echo $field;?><?php echo ';<br>';
 	   echo '?><br>';
 	   ?>
 		<?php echo '{!!';?> Form::bsRadioInline($list,$select,"{{$field}}","",[<?php echo $required;?>]) <?php echo '!!}';?><br>
@@ -45,7 +47,7 @@
 	  &#60;{{"?php"}}<br>
 	  <?php
 	   echo '$list = array('.$string_array.');<br>';
-	   echo '$select=0; <br>';
+	   echo '$select=$data->'; echo $field.';<br>';
 	   echo '?><br>';
 	   ?>
 		<?php echo '{!!';?> Form::bsRadioInline($list,$select,"{{$field}}","",[<?php echo $required;?>]) <?php echo '!!}';?><br>
@@ -55,15 +57,14 @@
 @endforeach
 <?php echo '{!!';?> Form::bsSubmit('Simpan',"") <?php echo '!!}';?> <br>
 &#60;&#47;form&#62;
-&#123!! Form::close() !!&#125  
 <hr>
-<b>SCRIPT VALIDATOR JS</b>
-<hr>
+<b>VALIDATOR JS</b> <br><br>
 var $validator = $("#form-{{$table_name}}").validate({ <br>
     ignore:[], <br>
     rules: { <br>
         @foreach ($columns as $value)
 		<?php
+		$type = $value->Type;
 		$field = $value->Field;
 		$required = $value->Null == "NO" ? 1 : 0;
 		$primary = $value->Key=="PRI" ? 1 : 0;
@@ -78,10 +79,9 @@ var $validator = $("#form-{{$table_name}}").validate({ <br>
      submitHandler: function(form) { <br>
       form.submit(); <br>
     }<br>
-});<br> <br><br>
-<hr>
-<b>SCRIPT POST CONTROLLER</b> 
-<hr><br>
+});<br>
+<br>
+<b>REQUEST POST CONTROLLER</b> <br><br>
 <?php
 $field_kunci = "";
 ?>
@@ -92,11 +92,10 @@ $field = $value->Field;
 $required = $value->Null == "NO" ? 1 : 0;
 $primary = $value->Key=="PRI" ? 1 : 0;
 ?>	
-
 @if($primary)
 <?php $field_kunci=$field;?>
+${{$field}}=Crypt::decrypt($request->input('{{$field}}'));<br>
 @endif
-
 @if(!$primary && $field!='created_at' && $field!='updated_at')
 ${{$field}}=$request->input('{{$field}}');<br>
 @endif
@@ -108,7 +107,7 @@ foreach($model_table as $md){
 	$nama_model.=ucfirst($md);
 }
 ?>
-$record = New {{$nama_model}}; <br>
+$record = {{$nama_model}}::find(${{$field_kunci}});<br>
 @foreach ($columns as $value)
 <?php
 $type = $value->Type;
@@ -127,15 +126,3 @@ $primary = $value->Key=="PRI" ? 1 : 0;
 @endif
 @endforeach
 $record->save();
-
-<hr>
-<b>SCRIPT Create Model</b>
-<hr><br>
-&#60;{{"?php"}}<br>
-namespace App\Models;<br>
-use Illuminate\Database\Eloquent\Model;<br>
-class {{$nama_model}} extends Model<br>
-{<br>
-    protected $primaryKey='{{$field_kunci}}';<br>
-    protected $table = '{{$table_name}}';<br>
-}<br>
